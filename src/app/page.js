a0be79227baf26d87769488629da40a8b30067ab
enessagroglu <enes-2001sagroglu@hotmail.com>
2025-04-15 21:36:53 +0300

    page grid yapısı kuruldu

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,38 @@
-
 import "./page.css";
+import { Col, Row } from "antd";
 
 export default function Home() {
   return (
-    <div >
-      home
+    <div className="root-page">
+      <Row>
+        <Col
+          className=""
+          span={2}
+          xs={{ order: 1 }}
+          sm={{ order: 2 }}
+          md={{ order: 3 }}
+          lg={{ order: 4 }}
+        ></Col>
+
+        <Col
+          className=""
+          span={2}
+          xs={{ order: 3 }}
+          sm={{ order: 4 }}
+          md={{ order: 2 }}
+          lg={{ order: 1 }}
+        ></Col>
+        <Col
+          className="test"
+          span={20}
+          xs={{ order: 4 }}
+          sm={{ order: 3 }}
+          md={{ order: 1 }}
+          lg={{ order: 2 }}
+        >
+          4 col-order-responsive
+        </Col>
+      </Row>
     </div>
   );
 }
