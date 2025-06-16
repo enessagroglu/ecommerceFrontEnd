feabb5c6aec80d7f5fc0cff2f04026ddb7f7f780
enessagroglu <enes-2001sagroglu@hotmail.com>
2025-06-16 22:16:52 +0300

    logo hazır, ürün sayfası gelişiyor

--- src/components/carousel/ProductCarousel.js
+++ src/components/carousel/ProductCarousel.js
@@ -1,33 +1,40 @@
-import { Carousel } from 'antd';
-import "../../app/products/products.css"
+import { Carousel } from "antd";
+import "../../app/products/products.css";
 import Image from "next/image";
 
-
-
-export default function ProductCarousel( { className, src1, src2, src3, src4, alt, width, height }) {
-
-    
-
+export default function ProductCarousel({
+  className,
+  src1,
+  src2,
+  src3,
+  src4,
+  alt,
+  width,
+  height,
+}) {
   return (
     <>
-      <Carousel className='product-carousel'>
-        <div className='product-image'>
-          <Image
-          className={className}
-          src={src1}
-          alt={alt}
-          width={width}
-          height={height}
-        />
+      <Carousel arrows infinite={false} className="product-carousel">
+        <div className="carousel-slide">
+          
+            <Image
+              className="carousel-image"
+              src={src1}
+              alt={alt}
+              width={width}
+              height={height}
+            />
+          </div>
+       
+
+        <div>
+          <h3>2</h3>
         </div>
         <div>
-          <h3 >2</h3>
+          <h3>3</h3>
         </div>
         <div>
-          <h3 >3</h3>
-        </div>
-        <div>
-          <h3 >4</h3>
+          <h3>4</h3>
         </div>
       </Carousel>
     </>
